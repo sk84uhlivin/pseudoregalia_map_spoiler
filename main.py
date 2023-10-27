4da4a78db946ee42a7b2a09d0dc1fc5625f826a1
sk84uhlivin <42231248+sk84uhlivin@users.noreply.github.com>
2023-10-27 12:09:47 -0400

    First upload

---
+++ main.py
@@ -0,0 +1,63 @@
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
+import map_coordinates
+import os
+import time
+import sys
+
+
+# Helper function for pyinstaller.
+def resource_path(relative_path):
+    """ Get absolute path to resource, works for dev and for PyInstaller """
+    try:
+        # PyInstaller creates a temp folder and stores path in _MEIPASS
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+
+    return os.path.join(base_path, relative_path)
+
+
+# Initialize two empty lists to store words and locations
+words = []
+locations = []
+coordinates = map_coordinates.locations
+
+# Open an Image
+img = Image.open(resource_path('map.png'))
+
+print("Generating text from spoiler_log.txt...")
+# Open the text document for reading
+with open('spoiler_log.txt', 'r') as file:
+    # Iterate through each line in the document
+    for line in file:
+        # Split the line at the colon
+        parts = line.strip().split(':')
+        if len(parts) == 2:
+            word, location = parts
+            # Add the word and location to their respective lists
+            words.append(word.strip())
+            locations.append(location.strip())
+
+for w, l in zip(words, locations):
+    # Check if the key exists in the dictionary and retrieve its value
+    if l in coordinates:
+        value = coordinates[l]
+
+        # Call draw Method to add 2D graphics in an image
+        I1 = ImageDraw.Draw(img)
+
+        # Custom font style and font size
+        myFont = ImageFont.truetype(resource_path('alittlepot.ttf'), 85)
+
+        # Add Text to an image
+        I1.text(xy=value, text=w, font=myFont, fill=(255, 0, 0), anchor="mm")
+
+    else:
+        print(f"'{w}' was not found in the dictionary.")
+print("Saving map...")
+# Save the edited image
+img.save("spoiler_map.png")
+print("Map saved, program will exit in 3 seconds.")
+time.sleep(3)
